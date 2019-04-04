5e65450b9ef40dec0d4b6a40f914f22feff542c2
Elma Rahman <elmarahman635@gmail.com>
2019-04-04 20:25:04 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,21 @@
 def addition(num1, num2)
+  puts "addition #{num1} + #{num2}"
+  adds num1 + num2
 end
 
 def subtraction(num1, num2)
+  puts "subract #{num1} - #{num2}"
+  subtracts num1 - num2
 end
 
 def division(num1, num2)
+  puts "divide #{num1}/#{num2}"
+  divides num1/num2
 end
 
 def multiplication(num1, num2)
+  puts "multiplies #{num1} * #{num2}"
+  multiples num1 * num2
 end
 
 def modulo(num1, num2)
